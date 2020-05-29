5b9fa809c419e2aae9e01e3330fc6c84e892c941
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-29 01:37:25 -0400

    added ejs route

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
+app.get('/', function(req,res) {
+    res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+    console.log('listing on port -- 3000 --');
+})
