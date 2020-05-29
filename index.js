efb27a50038efac71eb52e5d4ec4abf2a1d417ca
vladyslavnUA <vladyslavn@yahoo.com>
2020-05-29 14:47:54 -0400

    added port connection

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
     res.render('index.ejs');
 });
 
 io.sockets.on('connection', function(socket) {
     socket.on('username', function(username) {
         socket.username = username;
-        io.emit('is_online', '🙂 <i>' + socket.username + ' has joined the chat</i>');
+        io.emit('is_online', '🙂 <i>' + socket.username + ' join the chat..</i>');
     });
 
     socket.on('disconnect', function(username) {
-        io.emit('is_online', '😔 <i>' + socket.username + ' has left the chat</i>');
-    });
+        io.emit('is_online', '😔 <i>' + socket.username + ' left the chat..</i>');
+    })
 
-    socket.on('disconnect', function(username) {
+    socket.on('chat_message', function(message) {
         io.emit('chat_message', '<strong>' + socket.username + '</strong>: ' + message);
     });
+
 });
 
-app.listen(3000, () => {
-    console.log('listing on port -- 3000 --');
-})
+const server = http.listen(process.env.PORT || 8080, function() {
+    console.log('listening on *:8080');
+});
